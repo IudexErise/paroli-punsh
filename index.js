bac14d8e78ffcd74b67a8509a5e2190b58354b80
Vyacheslav Saprykin <vyacheslav.saprykin@gmail.com>
2023-01-25 12:50:27 +0600

    email validation

--- index.js
+++ index.js
@@ -16,3 +16,24 @@ closeSideMenu.addEventListener('click', removeSideMenu);
 sideMenuLinks.forEach((link) => {
   link.addEventListener('click', removeSideMenu)
 });
+
+let emailSend = document.querySelector('.footer_right_email-subscription_button');
+let emailInput = document.querySelector('.footer_right_email-subscription_input');
+let emailInputError = document.querySelector('.footer_right_email-subscription_error-message');
+let emailCheckbox = document.querySelector('#email_checkbox');
+
+function validateEmail() {
+  if (emailInput.checkValidity() && emailCheckbox.checked) {
+    emailInput.classList.remove('error');
+    alert('Вы успешно подписались!');
+    emailInputError.innerHTML = '';
+  } else if (emailCheckbox.checked) {
+      emailInput.classList.add('error');
+      emailInputError.innerHTML = emailInput.validationMessage;
+    } else {
+      emailInput.classList.add('error');
+      emailInputError.innerHTML = 'Пожалуйста, дайте согласие на обработку данных.';
+    }
+}
+
+emailSend.addEventListener('click', validateEmail);
